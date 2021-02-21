174cc018d87c4f8a1c3cc9c72e34e17c9bd6a410
Charles Robinson <charlesdalerobinson@gmail.com>
2021-02-21 13:38:16 -0800

    Added employee directory header to employee directory.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <p className="App-title"> Employee Directory</p>
+      <p className="App-subtitle">Enter a name in the search field to find an employee or click on the name field to sort employee directory in ascending or descending order.</p>
+      {/* Input search button here... */}
+      {/* employee directory here... */}
     </div>
   );
 }
