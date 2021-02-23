de92a4d55bc7d936b3ad433eb13eedd5a093f34a
Charles Robinson <charlesdalerobinson@gmail.com>
2021-02-23 14:52:00 -0800

    Added input search field for user to search employee directory. Added prevent default onKeyPress to prevent user from hitting enter, thus refreshing the page and grabbing new employees from the original API call.

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends React.Component {
   state = {
     loading: true,
     employees: [],
-    filteredEmployees: []
+    filteredEmployees: [],
+    search: ""
   }
 
   async componentDidMount() {
@@ -21,6 +22,20 @@ class App extends React.Component {
     console.log(this.state.employees[0].name.first);
   }
 
+  handleInputChange = (event) => {
+    event.preventDefault();
+
+    // Updating the input's state
+    this.setState({ search: event.target.value });
+    
+    // const filteredArray = this.state.filteredEmployees.filter(function (el) {
+    //   return el.first === this.state.search
+    // })
+
+    // this.setState ({ filteredEmployees: filteredArray})
+
+  };
+
   render() {
 
     return (
@@ -29,6 +44,16 @@ class App extends React.Component {
           <p className="App-title"> Employee Directory</p>
           <p className="App-subtitle">Enter a name in the search field to find an employee or click on the name field to sort employee directory in ascending or descending order.</p>
           {/* Input search button here... */}
+          <form className="App-form">
+            <input
+              value={this.state.search}
+              name="search"
+              onChange={this.handleInputChange}
+              type="text"
+              onKeyPress={e => { if (e.key === 'Enter') e.preventDefault()}}
+            // placeholder="Search in here..."
+            />
+          </form>
         </div>
         <div className="App-employee-list">
           {/* employee directory here... */}
